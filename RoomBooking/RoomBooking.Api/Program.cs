35ee18295dbf44f54f95cb238c4d34aae67e10c6
Kevin Sousselier <kevin.sousselier@gmail.com>
2022-02-11 21:47:18 +0100

    Fix CORS

--- RoomBooking/RoomBooking.Api/Program.cs
+++ RoomBooking/RoomBooking.Api/Program.cs
@@ -41,7 +41,7 @@ builder.Services.AddSingleton(mapper);
 builder.Services.AddCors(options =>
 {
 options.AddPolicy("MyOrigin",
-    builder => builder.WithOrigins("localhost:3000")
+    builder => builder.WithOrigins("http://localhost:3000")
                       .AllowAnyMethod()
                       .AllowAnyHeader());
 });
